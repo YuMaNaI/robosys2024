26615825c1c65e500eff9791ebee4654b0c7ba2c
Yuma Naito <n.yuma0623@gmail.com>
2024-11-07 12:15:34 +0900

    C

--- test.bash
+++ test.bash
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/bin/bash -xv
 
 ng () {
 	echo ${1}行目が違うよ
@@ -9,6 +9,15 @@ res=0
 
 out=$(seq 5 | ./plus)
 [ "${out}" = 15 ] || ng "$LINENO"
-
-[ "${res}" = 0 ] && echo OK
+  　 
+### STRANGE INPUT ###
+out=$(echo あ | ./plus)           #計算できない値を入力してみる
+[ "$?" = 1 ]      || ng "$LINENO" #終了ステータスが1なのを確認
+[ "${out}" = "" ] || ng "$LINENO" #この行と上の行は入れ替えるとダメです
+  　                                      #（↑なぜかは考えてみましょう）
+out=$(echo | ./plus)              #なにも入力しない
+[ "$?" = 1 ]      || ng "$LINENO" #これも異常終了する
+[ "${out}" = "" ] || ng "$LINENO"
+  　 
+[ "$res" = 0 ] && echo OK
 exit $res
